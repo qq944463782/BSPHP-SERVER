a7461f63aeae5f0170ea9f681ff43c4e80df3780
enzu zhou <enzuzhou@enzudeMac-mini.local>
2026-03-28 02:25:18 +0800

    优化更新细节

--- LibBsphp/common/hooks.function.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BSPHP_SET') or die('Not,This File Not Can in Ie Open');
-function BsRun_Plug($Run_Name, $param)
-{
-}
-?>
--- LibBsphp/common/version.function.php
+++ LibBsphp/common/version.function.php
@@ -1,5 +1,5 @@
 <?php
-define('BSPHP_VERSION','RSA v1.0.0');
+define('BSPHP_VERSION','v1.0.0');
 define('BSPHP_VERSION_ID','20260328');
 define('BSPHP_VERSION_TIME','20260328');
 ?>
