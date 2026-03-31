cf3e9d36da50bf14915ff228e09c852f24c71946
enzu zhou <enzuzhou@enzudeMac-mini.local>
2026-03-31 14:42:35 +0800

    优化更新不存在函数

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,8 @@
 <?php
 $reqUri=$_SERVER['REQUEST_URI'] ?? '';
 $path=parse_url($reqUri, PHP_URL_PATH) ?? '';
-if ($path !=='' && substr($path, -1) !=='/' && str_ends_with($path, '/admin')) {
+$adminSuffix='/admin';
+if ($path !=='' && substr($path, -1) !=='/' && substr($path, -strlen($adminSuffix))===$adminSuffix) {
 $q=parse_url($reqUri, PHP_URL_QUERY);
 $loc=$path . '/index.php' . ($q !==null && $q !=='' ? '?' . $q : '');
 header('Location: ' . $loc, true, 302);
--- agent/index.php
+++ agent/index.php
@@ -1,7 +1,8 @@
 <?php
 $reqUri=$_SERVER['REQUEST_URI'] ?? '';
 $path=parse_url($reqUri, PHP_URL_PATH) ?? '';
-if ($path !=='' && substr($path, -1) !=='/' && str_ends_with($path, '/agent')) {
+$agentSuffix='/agent';
+if ($path !=='' && substr($path, -1) !=='/' && substr($path, -strlen($agentSuffix))===$agentSuffix) {
 $q=parse_url($reqUri, PHP_URL_QUERY);
 $loc=$path . '/index.php' . ($q !==null && $q !=='' ? '?' . $q : '');
 header('Location: ' . $loc, true, 302);
