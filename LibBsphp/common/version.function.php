94770d80c3be8505cf02e8a6e8748a1ce0d62c65
enzu zhou <enzuzhou@enzudeMac-mini.local>
2026-08-02 12:19:28 +0800

    优化更新api客户通信兼容pro版本m参数验证，软件配置显示最小的

--- LibBsphp/common/version.function.php
+++ LibBsphp/common/version.function.php
@@ -1,5 +1,5 @@
 <?php
-define('BSPHP_VERSION','v1.0.1');
-define('BSPHP_VERSION_ID','20260702');
-define('BSPHP_VERSION_TIME','20260702');
+define('BSPHP_VERSION','v1.0.3');
+define('BSPHP_VERSION_ID','20260802');
+define('BSPHP_VERSION_TIME','20260802');
 ?>
